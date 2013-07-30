f2f8837bfd8d55db1c82c81c02cc4042f6fe3d9b
Johannes de Fine Licht <johannes@musicmedia.dk>
2013-07-30 18:21:21 +0200

    Remove ignored files

--- Matlab/stepsperlaunch_benchmark.m
+++
@@ -1,7 +0,0 @@
-function [] = stepsperlaunch_benchmark()
-  data = csvread('/Users/johannes/Dropbox/Bachelor/TrackingPositrons/Simulation/Data/stepsperlaunch_benchmark',1);
-  plot(data(:,1),data(:,2),'-xg');
-  title('Steps per kernel launch benchmark');
-  xlabel('Steps per kernel launch');
-  ylabel('Runtime [s]');
-end
